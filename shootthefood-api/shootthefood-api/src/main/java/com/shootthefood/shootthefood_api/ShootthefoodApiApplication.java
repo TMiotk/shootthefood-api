21c533c091dfaf574d24528db8b21cf0bac7afd5
Tomasz Miotk <thomas.miotk86@gmail.com>
2025-12-07 12:52:47 +0100

    project init

---
+++ shootthefood-api/shootthefood-api/src/main/java/com/shootthefood/shootthefood_api/ShootthefoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.shootthefood.shootthefood_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShootthefoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShootthefoodApiApplication.class, args);
+	}
+
+}
---
+++ shootthefood-api/shootthefood-api/src/test/java/com/shootthefood/shootthefood_api/ShootthefoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shootthefood.shootthefood_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShootthefoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
